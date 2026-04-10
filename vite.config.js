8f746fef69bfe9570dca68bd914db7ec03ccfcfd
jean862 <3054463394@qq.com>
2026-04-10 11:56:03 +0800

    修改为新仓库vis并重新打包

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 🌟 核心 1：配置基础路径。如果你的 GitHub 仓库名叫 "ai-vis-project"
+  // 这里必须写成 '/ai-vis-project/' (前后都要有斜杠)
+  base: '/vis/', 
+  
+  // 🌟 核心 2：将打包文件夹从 dist 改为 docs，为了迎合 GitHub Pages
+  build: {
+    outDir: 'docs'
+  },
+  
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
